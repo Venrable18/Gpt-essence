dbadf272ae8613fcf585a0ebbf868c146617a7e0
Onen_victor <87070469+Venrable18@users.noreply.github.com>
2025-02-10 16:40:17 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const chatContainer = document.querySelector(".chat-container");
 const themeButton = document.querySelector("#theme-btn");
 const deleteButton = document.querySelector("#delete-btn");
 let userText = null;
-const API_KEY = "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";
+var API_KEY = "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";
 const loadDataFromLocalstorage = () => {
     // Load saved chats and theme from local storage and apply/add on the page
     const themeColor = localStorage.getItem("themeColor");
