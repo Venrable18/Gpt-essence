0fcb911a94fa66bbd4b47826d1c5a88658ca3773
Onen_victor <87070469+Venrable18@users.noreply.github.com>
2025-02-11 04:59:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-HuTheconst chatInput = document.querySelector("#chat-input");
+const chatInput = document.querySelector("#chat-input");
 const sendButton = document.querySelector("#send-btn");
 const chatContainer = document.querySelector(".chat-container");
 const themeButton = document.querySelector("#theme-btn");
